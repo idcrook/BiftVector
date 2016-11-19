d55959da70cd7a43bcb9a26a8b1e3737b0a51929
David Crook <idcrook@users.noreply.github.com>
2016-11-19 08:20:49 -0700

    Checking into shared Git repo
     - added a CustomStringConvertible extension
     - Unit test matching

--- Sources/BiftVector.swift
+++ Sources/BiftVector.swift
@@ -1,4 +1,69 @@
+
+// API
+//
+// Constructor
+//  - BiftVector(size: 0)
+//  - BiftVector(bitlist: [1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1])
+//  - BiftVector(bitlist: "1010010100101001")
+//  - BiftVector(intVal: 8675309)
+//  - BiftVector(intVal: 8675309, size: 24)
+//  - BiftVector(textString: "hello")
+//  - BiftVector(hexString: "decafBAD")
+//  - bv2 = b1.copy()  // Copy constructor
+//
+//
+// Display
+//  - print(bv)
+//  - let d = bv.description
+//  - led dd = bv.debugDescription
+//  - let i = Int(bv)  // may return an array of Ints
+//
+// Accessing and setting bits / slices
+//  - let bit_1_or_0 = bv[M]
+//  - let sliceBv = bv[M:N]
+//  - for bit in biftvec { print ("\(bit)") }
+//
+// Logical opertions
+//  - bvr = bv1 ^ bv2     // Bitwise Logical XOR
+//  - bvr = bv1 & bv2     // Bitwise Logical AND
+//  - bvr = bv1 | bv2     // Bitwise Logical OR
+//  - bvr = ~bv1          // Bitwise negation
+//
+// BiftVector comparisons
+//  - bv1 == bv2
+//  - bv1 != bv2
+//  - bv1 <  bv2
+//  - bv1 <= bv2
+//  - bv1 >  bv2
+//  - bv1 >= bv2
+//
+// // Add advance operations tier 2
+//
+
+
 struct BiftVector {
 
-    var text = "Hello, World!"
+    var _bv: [Int]?
+    var size: Int = 0
+    
+    init? (size: Int) {
+        guard size >= 0 else {
+            // Error:
+            return
+        }
+        _bv = [Int]()
+        
+    }
+    
+    
+}
+
+extension BiftVector : CustomStringConvertible {
+    
+    var description: String {
+        guard size >= 0 else {
+            return "Error: BiftVector size less than zero."
+        }
+        return "FIXME"
+    }
 }
--- Tests/BiftVectorTests/BiftVectorTests.swift
+++ Tests/BiftVectorTests/BiftVectorTests.swift
@@ -5,9 +5,8 @@ class BiftVectorTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(BiftVector().text, "Hello, World!")
+        XCTAssertEqual(BiftVector(size: 0)!.description, "FIXME")
     }
-
 
     static var allTests : [(String, (BiftVectorTests) -> () throws -> Void)] {
         return [
