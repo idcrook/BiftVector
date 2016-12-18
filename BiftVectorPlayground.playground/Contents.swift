17e760ba27ea596ec029a71aeca600cf364f593f
David Crook <idcrook@users.noreply.github.com>
2016-12-18 12:37:02 -0700

    added playground

---
+++ BiftVectorPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import BiftVector
+
+var str = "Hello, playground"
+
+let bv1 = BiftVector(hexString: "0b0a0d0e", withSize: 32)
+let bv2 = BiftVector(textString: "2145")
+let bv3 = bv1 | bv2
+
+
+
